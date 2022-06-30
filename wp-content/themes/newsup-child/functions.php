a36cb3f1437f8ea5e1b9190c7663f908dc6400dd
Sjoerd <sjoerd@mybusiness.nl>
2022-06-30 17:05:06 +0200

    - Default colors linked to css-variables

--- wp-content/themes/newsup-child/functions.php
+++ wp-content/themes/newsup-child/functions.php
@@ -2,13 +2,21 @@
 
 function wittebrug_enqueue_styles()
 {
+    $bootstrap = 'bootstrap';
+    $bootstrap_smartmenus = 'smartmenus';
+    $newsup_style = 'newsup-style';
+    $newsup_default = 'newsup-default';
+    $newsup_fonts = 'newsup-fonts';
     $parent_style = 'parent-style';
     $child_style = 'child-style';
+    $fontawesome_5 = 'font-awesome-5-all';
+    $fontawesome_4 = 'font-awesome-4-shim';
+    $owl_carousel = 'owl-carousel';
 
     wp_register_style($parent_style, get_template_directory_uri() . '/style.css');
     wp_register_style($child_style,
         get_template_directory_uri() . '/style.css',
-        array($parent_style),
+        array($parent_style, $bootstrap, $newsup_default, $newsup_fonts),
         wp_get_theme()->get('Version')
     );
 
